10a708b8b649bc992f779cceb1a95b4b6a2ed83f
Eilis <e.j.hannon@exeter.ac.uk>
2017-04-03 15:59:40 +0100

    Collate sep files into r object

---
+++ Scripts/collateResults.r
@@ -0,0 +1,24 @@
+### sumarize results of gene x methylation correlations
+
+setwd("/mnt/data1/Helen/GExpxDNAm/")
+
+all.output<-list.files(path = "Output", pattern = "Cor_GExp_Hydroxy_Matrix")
+
+cor.pearson<-NULL
+for(file in all.output[-grep("Rank", all.output)]){
+	tmp<-read.table(paste("Output/", file, sep = ""), header = TRUE)
+	tmp<-t(tmp)
+	cor.pearson<-rbind(tmp, cor.pearson)
+}
+save(cor.pearson, file = "Output/All_Pearson_Cor_Hydroxy.rdata")
+
+all.output<-list.files(path = "Output", pattern = "Cor_GExp_HYOX_Matrix")
+
+cor.rank<-NULL
+for(file in all.output[grep("Rank", all.output)]){
+	tmp<-read.table(paste("Output/", file, sep = ""), header = TRUE)
+	tmp<-t(tmp)
+	cor.rank<-rbind(tmp, cor.rank)
+}
+
+save(cor.rank, file = "Output/All_Rank_Cor_DNAm.rdata")
